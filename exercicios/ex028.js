01ff2d89fae26841f2a92dec53b9e07f78b932b5
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-01-30 21:11:05 -0300

    Jogo da adivinhação

---
+++ exercicios/ex028.js
@@ -0,0 +1,20 @@
+const input = require('prompt-sync')({sigint: true})
+
+console.log('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+
+function rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const numComputador = rande(0, 5)
+const numPessoa = Number(input('Em que número eu pensei? '))
+
+setTimeout(() => {
+    console.log('PROCESSANDO...')
+    
+    if(numComputador === numPessoa) {
+        console.log('Parabéns, você acertou, eu penseu no número ' + numComputador)
+    } else {
+        console.log(`GANHEI! Eu pensei no número ${numComputador} e não no ${numPessoa}`)
+    }
+}, 1000)
